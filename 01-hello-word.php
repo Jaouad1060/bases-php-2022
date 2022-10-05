ee281769c8b2c38df3a74e5c6c43587989368706
Jaouad1060 <jaouad1060@hotmail.com>
2022-10-05 11:49:27 +0200

    hello world

---
+++ 01-hello-word.php
@@ -0,0 +1,29 @@
+<?php
+// commentaire sur une ligne
+
+// on peux mettre des balises php n'importe ou dans une âge html
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World</title>
+</head>
+<body>
+   
+   <?php // ouberture de php
+
+    // affiche la lignz : echo "text" ;
+
+    echo "<h1>Hello World</h1>";
+
+   //fermeture de php ?> 
+
+</body>
+</html>
